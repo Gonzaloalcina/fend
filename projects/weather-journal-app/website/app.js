5f8cd3bcc95f89f0f7204ed9f1f7544a9a9fc53a
Gonzalo-Alcina <gonzalo.alcinabenavente@gmail.com>
2020-09-16 17:14:03 +0200

    Store URL and apiKey with conts
    better!

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,6 +1,6 @@
 /* Global Variables */
-let baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=';
-let apiKey = '0d20ff942186c94d0f12c1bc0f393ab8';
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '0d20ff942186c94d0f12c1bc0f393ab8';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
