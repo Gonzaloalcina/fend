48377a42b9509317bb5c0fe0cf986b4ee691edac
Gonzalo-Alcina <gonzalo.alcinabenavente@gmail.com>
2020-10-14 12:08:49 +0200

    Install plugins in dev and prod
    npm i -D @babel/core @babel/preset-env babel-loader
    npm i -D style-loader node-sass css-loader sass-loader
    npm i -D clean-webpack-plugin
    npm i -D html-webpack-plugin
    npm i -D mini-css-extract-plugin
    npm i -D optimize-css-assets-webpack-plugin terser-webpack-plugin

--- projects/evaluate-news-nlp/webpack.prod.js
+++ projects/evaluate-news-nlp/webpack.prod.js
@@ -1,16 +1,31 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    optimization: {
+        minimizer: [new OptimizeCssAssetsWebpackPlugin(), new TerserPlugin()],
+    },
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: '/\.scss$/',
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+            },
+            {
+                test: '/\.html$/',
+                use: "html-loader"
             }
         ]
     },
@@ -18,6 +33,16 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].css" }),
     ]
 }
