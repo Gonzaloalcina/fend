7328935e2366791eb535ed646b7fe39bf3edcd5a
Gonzalo-Alcina <gonzalo.alcinabenavente@gmail.com>
2020-11-16 18:18:20 +0100

    Setup Service Workers

--- projects/evaluate-news-nlp/webpack.prod.js
+++ projects/evaluate-news-nlp/webpack.prod.js
@@ -5,6 +5,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -48,5 +49,28 @@ module.exports = {
             protectWebpackAssets: false
         }),
         new MiniCssExtractPlugin({ filename: "[name].css" }),
-    ]
-}
+        new WorkboxPlugin.GenerateSW({
+            // Do not precache images
+            exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+      
+            // Define runtime caching rules.
+            runtimeCaching: [{
+              // Match any request that ends with .png, .jpg, .jpeg or .svg.
+              urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+      
+              // Apply a cache-first strategy.
+              handler: 'CacheFirst',
+      
+              options: {
+                // Use a custom cache name.
+                cacheName: 'images',
+      
+                // Only cache 10 images.
+                expiration: {
+                  maxEntries: 10,
+                },
+              },
+            }],
+          })
+        ]
+    };
