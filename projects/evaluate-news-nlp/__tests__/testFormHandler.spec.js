d2eeaea3b254213eb3fd935ad5d2f367747ef138
Gonzalo-Alcina <gonzalo.alcinabenavente@gmail.com>
2020-11-16 09:44:54 +0100

    testing UI code

--- projects/evaluate-news-nlp/__tests__/testFormHandler.spec.js
+++ projects/evaluate-news-nlp/__tests__/testFormHandler.spec.js
@@ -1,14 +1,19 @@
 // Import the js file to test
-import { handleSubmit } from "../src/client/js/formHandler"
+import { updateUI } from "../src/client/js/formHandler";
+
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 
  
 describe("Testing the submit functionality", () => {
-    test("testing succes", () => {
-        const input = "www.google.com";
-        expect(handleSubmit).toBe(true);
-    });
-    test("testing error", () => {
-        const input = "h o l a";
-        expect(handleSubmit).toBe(false);
+    test("testing the updating of the UI", () => {
+        const dom = new JSDOM(`<div id="UI"></div>`);
+        const content = { 
+            data.score_tag: "P",
+        };
+        const element = dom.window.document.getElementById("UI");
+
+        updateUI(element,content);
+        expect(element.innerHTML).toBe(`Polarity: P`);
     });
 });
