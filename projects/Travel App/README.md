49e323063fb077a9f96d7a703e8c5d86c452d3c0
Gonzalo-Alcina <gonzalo.alcinabenavente@gmail.com>
2020-12-01 09:50:18 +0100

    docs: Duplicate weather journal app folder

---
+++ projects/Travel App/README.md
@@ -0,0 +1,34 @@
+# Weather-Journal App Project
+
+## Third project of the Udacity's Front End Nanodegree
+
+### Overview
+The project requires you to create an asynchronous web app that uses Web API and user data to dynamically update the UI. 
+
+### Steps
+* Set up a local server in `<server.js>` installing Node, Express, Body-Parser and Cors.
+* Add a GET route that returns the `<projectData>`object in the server code. The, add a POST route that adds incoming data tp `<projectData>`.
+
+* Use credentials from OpenWeatherMap
+* Set the `<app.js>` using
+    * `<async>` functions
+    * `<fetch>`
+    * GET and POST requests
+* Update the UI with data that includes
+    * temperature
+    * date
+    * user response
+
+### Tools
+* Visual Studio Code
+* Notion
+* OpenWeatherMap
+* [Coolors](https://coolors.co/)
+* [Html Colors](https://htmlcolorcodes.com/)
+* [Google Fonts](https://fonts.google.com/)
+
+### Result
+![Result](https://github.com/Gonzaloalcina/fend/blob/refresh-2019/projects/weather-journal-app/website/Gif%20result/Weather%20Journal%20App.gif)
+
+
+
---
+++ projects/Travel App/website/app.js
@@ -0,0 +1,83 @@
+/* Global Variables */
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '0d20ff942186c94d0f12c1bc0f393ab8';
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+//let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.toLocaleString('en-US', { month: 'long', day: 'numeric', year:'numeric'});
+
+document.getElementById('generate').addEventListener('click', action);
+
+function action(e){
+    const postalCode = document.getElementById('zip').value;
+    const feelings = document.getElementById('feelings').value;
+    console.log(newDate);
+ 
+    getTemp(baseUrl,postalCode, apiKey)
+    .then(function (data){
+         if(String(data.cod) === '404'){
+            alert('Wrong zip code, try again!');
+        } else {
+            postData('http://localhost:8000/addData', { date: newDate, temp: data.main.temp, feel: feelings})
+            .then(function(){
+            //User Interface
+            updateUI()
+        })}  
+    })
+}
+
+
+
+//Async GET
+const getTemp = async (baseUrl, postalCode, apiKey) => {
+    const response = await fetch(baseUrl + postalCode + ',us' + '&units=metric' + '&APPID=' + apiKey);
+    console.log(response);
+    try {
+        const data = await response.json();
+        console.log(data);
+        console.log('ok get');
+        return data;
+    }
+    catch (error) {
+        console.log('error', error);
+    }
+}
+
+//Async POST
+const postData = async (url = '', data = {}) => {
+    const postRequest = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+    try {
+        console.log('ok post');
+        const newData = await postRequest.json();
+        console.log(newData, 'ok post vel');
+        return newData;
+    }
+    catch(error){
+        console.log('error', error);
+    }
+}
+
+//User Interface 
+const updateUI = async () => {
+    const request = await fetch('http://localhost:8000/getData');
+    try {
+        const allData = await request.json();
+        console.log('Updating UI');
+        document.getElementById('date').innerHTML = allData.date;
+        document.getElementById('temp').innerHTML = `${allData.temp}°`;
+        document.getElementById('content').innerHTML = allData.feel;
+    }
+    catch(error) {
+        console.log('error, error');
+    }
+}
+
+
