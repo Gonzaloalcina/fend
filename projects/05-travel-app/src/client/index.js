5c096f299049b308f9c278fcabb91ef1dc78540c
Gonzalo-Alcina <gonzalo.alcinabenavente@gmail.com>
2020-12-11 22:56:29 +0100

    feat: Install packages

---
+++ projects/05-travel-app/src/client/index.js
@@ -0,0 +1,13 @@
+import {action} from './js/app';
+import {getTemp} from './js/app';
+import {postData} from './js/app';
+import {updateUI} from './js/app';
+
+import './styles/style.scss';
+
+export {
+    action,
+    getTemp,
+    postData,
+    updateUI
+}
---
+++ projects/05-travel-app/webpack.dev.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: 'projects/05-travel-app/src/client/index.js',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
+    mode: 'development',
+    devtool: 'source-map',
+    stats: 'verbose',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"],
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "projects/05-travel-app/src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        })
+    ]
+}
